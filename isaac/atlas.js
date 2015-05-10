86ec183e7b3f57f2d93e814fe761148c7abd739b
laixintao <laixintao1995@163.com>
2015-05-11 02:27:07 +0800

    添加以撒Demo

---
+++ isaac/atlas.js
@@ -0,0 +1,55 @@
+
+function creat_active_tools_atlas(id,type,en_name,ch_name,charge_time,effect){
+	var active_tools_atlas = new Object();
+	active_tools_atlas.id = id;
+	active_tools_atlas.type=type;
+	active_tools_atlas.en_name=en_name;
+	active_tools_atlas.ch_name=ch_name;
+	active_tools_atlas.charge_time=charge_time;
+	active_tools_atlas.effect=effect;
+	return active_tools_atlas;
+}
+
+function get_active_tools_atlas(){
+	var all_atlas = new Array();
+
+	for(var i=0;i<73;i++){
+		var temp = "atlas.active_tools#at"+i;
+		var id = i;
+		var ch_name = $(temp+" > ch_name").text();
+		var type = $(temp+" > type").text();
+		var en_name = $(temp+" > en_name").text();
+		var charge_time = $(temp+" > charge_time").text();
+		var effect = $(temp+" > effect").text();
+		all_atlas.push(creat_active_tools_atlas(id,type,en_name,ch_name,charge_time,effect));
+	}
+	return all_atlas;
+}
+
+
+
+function creat_passive_tools_atlas(id,type,en_name,ch_name,effect){
+	var active_tools_atlas = new Object();
+	active_tools_atlas.id = id;
+	active_tools_atlas.type=type;
+	active_tools_atlas.en_name=en_name;
+	active_tools_atlas.ch_name=ch_name;
+	active_tools_atlas.effect=effect;
+	return active_tools_atlas;
+}
+
+
+function get_passive_tools_atlas(){
+	var all_atlas = new Array();
+
+	for(var i=0;i<184;i++){
+		var temp = "atlas.passive_tools#at"+i;
+		var id = i;
+		var ch_name = $(temp+" > ch_name").text();
+		var type = $(temp+" > type").text();
+		var en_name = $(temp+" > en_name").text();
+		var effect = $(temp+" > effect").text();
+		all_atlas.push(creat_passive_tools_atlas(id,type,en_name,ch_name,effect));
+	}
+	return all_atlas;
+}
