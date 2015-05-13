aa549e3af6ca71fe228628b0edec5afc69a811ae
laixintao <laixintao1995@163.com>
2015-05-14 03:24:40 +0800

    update isaac

--- isaac/main.js
+++ isaac/main.js
@@ -1,9 +1,13 @@
 var active_tools;
 var Accessories;
+var cards;
+var shipin;
 
 $().ready(function(){
 	active_tools = get_active_tools_atlas();
 	Accessories = get_Accessories();
+	cards = get_Cards();
+	shipin = get_shipin();
 	add_Listener();
 });
 
@@ -15,7 +19,12 @@ function add_Listener(){
 	$("#Accessories > img").click(function(){
 	on_Click_Listener(this);
 	});
-
+	$("#cards > img").click(function(){
+	on_Click_Listener(this);
+	});
+	$("#shipin > img").click(function(){
+	on_Click_Listener(this);
+	});
 	$("#close > img").click(function(){
 		$("#board").css("display","none");
 	});
@@ -52,7 +61,29 @@ function on_Click_Listener(myclick){
 		if(Accessories[click_id].unlock.length>0)
 			str+="<br /><span style='background-color: #FFD700;'><strong>解锁方式：</strong></span>"+Accessories[click_id].unlock;
 		str+="</p><div>";
-	}
+	}else if(this_class==="cards"){
+		str = "<div class='temp' style='float:left;'><img style='height: 60px;width: 47px;margin: 5px;' src='graphics/cards/"+
+		num+".jpg'></div><div class='temp' style='font-size:18px;float:left;'><p ><strong>"+
+
+		cards[click_id].ch_name+"("+cards[click_id].atype+")</strong><br />"+
+		"<i>"+cards[click_id].en_name+"</i><br /></p></div>"+
+
+		"<div class='temp' style='color:#8DB2C9;clear:both;'><span style='background-color: #FFD700;'><strong>效果：</strong></span>"+cards[click_id].effect;
+		if(cards[click_id].unlock.length>0)
+			str+="<br /><span style='background-color: #FFD700;'><strong>解锁方式：</strong></span>"+cards[click_id].unlock;
+		str+="</p><div>";
+	}else if(this_class==="shipin"){
+    str = "<div class='temp' style='float:left;'><img style='max-height: 50px;max-width: 50px;width: 50px;margin: 5px;' src='graphics/shipin/"+
+		num+".png'></div><div class='temp' style='font-size:18px;float:left;'><p ><strong>"+
+
+    shipin[click_id].ch_name+"("+shipin[click_id].atype+")</strong><br />"+
+    "<i>"+shipin[click_id].en_name+"</i><br /></p></div>"+
+
+    "<div class='temp' style='color:#8DB2C9;clear:both;'><span style='background-color: #FFD700;'><strong>效果：</strong></span>"+shipin[click_id].effect;
+    if(shipin[click_id].unlock.length>0)
+      str+="<br /><span style='background-color: #FFD700;'><strong>解锁方式：</strong></span>"+shipin[click_id].unlock;
+    str+="</p><div>";
+  }
 	
 
 	$("#display > .temp").detach();  //先移除之前的元素，在添加新的
